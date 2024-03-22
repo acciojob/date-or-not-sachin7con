5f79a1886b690e4f2b2cdd684f17a8b5d09b91cb
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-03-22 14:43:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 var isDate = function (input) {
-  //   write your code here
+  //SGN   write your code here
+	  if (Object.prototype.toString.call(input) === "[object Date]") {
+    // If it's a valid Date object, return true
+    return !isNaN(input.getTime());
+  }
+
+  // Check if input is a string
+  if (typeof input === 'string' || input instanceof String) {
+    // Attempt to parse the string into a Date
+    const parsedDate = new Date(input);
+    // Check if parsing was successful and it's a valid date
+    return !isNaN(parsedDate.getTime());
+  }
+
+  // If input is neither Date object nor string, return false
+  return false;
+
+	
 };
 
 // Do not change the code below.
